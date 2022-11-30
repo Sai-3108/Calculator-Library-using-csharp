986ab5c4b15bf4c2fcd3fb60483a68a3036d2855
Sai <sainadh3108@gmail.com>
2022-11-30 10:40:59 +0530

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using CalculatorLibrary;
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string? restart_calculator = "no";
+            string[] operator_Array = new string[4] { "+", "-", "*", "/" };
+            double number1;
+            double number2;
+            do
+            {
+                Console.WriteLine("   MILESTONE CALCULATOR    ");
+                Console.WriteLine("[ + ] --> Addition");
+                Console.WriteLine("[ - ] --> Substraction");
+                Console.WriteLine("[ * ] --> Multiplication");
+                Console.WriteLine("[ / ] --> Division");
+                Console.WriteLine("Choose Your Operation :");
+                string? oprtr = Console.ReadLine();
+
+                while (!operator_Array.Contains(oprtr))
+                {
+                    Console.WriteLine("  Wrong Input" + "\n " + " Available operations are  + , - , * , / ");
+                    Console.WriteLine("Choose Your Operation :");
+                    oprtr = Console.ReadLine();
+                }
+                Console.WriteLine("Enter The First Number :");
+                string? input = Console.ReadLine();
+
+                while (Double.TryParse(input, out double n) == false)
+                {
+                    Console.WriteLine("Enter Actual Number");
+                    input = Console.ReadLine();
+                    input = Console.ReadLine();
+                }
+                number1 = Double.Parse(input);
+                Console.WriteLine("Enter The Second Number :");
+                string? input2 = Console.ReadLine();
+                while (Double.TryParse(input2, out double n) == false)
+                {
+                    Console.WriteLine("Enter Actual Number");
+                    input2 = Console.ReadLine();
+                }
+                number2 = Double.Parse(input2);
+
+                if (oprtr == "+")
+                {
+                    Console.WriteLine("The Answer is :" + Class1.Addition(number1, number2));
+                    Console.WriteLine("Write 'yes'  to continue ");
+                    restart_calculator = Console.ReadLine();
+                }
+                else if (oprtr == "-")
+                {
+                    Console.WriteLine("The Answer is :" + Class1.Substraction(number1, number2));
+                    Console.WriteLine("Write 'yes'  to continue ");
+                    restart_calculator = Console.ReadLine();
+                }
+                else if (oprtr == "*")
+                {
+                    Console.WriteLine("The Answer is :" + Class1.Multiplication(number1, number2));
+                    Console.WriteLine("Write 'yes'  to continue ");
+                    restart_calculator = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("The Answer is :" + Class1.Division(number1, number2));
+                    Console.WriteLine("Write 'yes'  to continue ");
+                    restart_calculator = Console.ReadLine();
+                }
+            }
+            while (restart_calculator == "yes" || restart_calculator == "y");
+            if (restart_calculator != "yes")
+            {
+
+                Console.WriteLine(" See you Again !! Bye Bye ");
+
+            }
+        }
+    }
+}
---
+++ CalculatorLibrary/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CalculatorLibrary
+{
+     public class Class1
+    {
+        public static double Addition(double x, double y)
+        {
+            double number1 = x;
+            double number2 = y;
+            double answer = number1 + number2;
+            return answer;
+        }
+        public static double Substraction(double x, double y)
+        {
+            double number1 = x;
+            double number2 = y;
+            double answer = number1 - number2;
+            return answer;
+        }
+        public static double Multiplication(double x, double y)
+        {
+            double number1 = x;
+            double number2 = y;
+            double answer = number1 * number2;
+            return answer;
+        }
+        public static double Division(double x, double y)
+        {
+            double number1 = x;
+            double number2 = y;
+            double answer = number1 / number2;
+            return answer;
+
+        }
+    }
+}
